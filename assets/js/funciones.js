f30d6bf10aa3fe8c7cceed49fd00d0ee0e4bba76
cspcapo <117932584+cspcapo@users.noreply.github.com>
2023-05-22 08:47:23 -0300

    Add files via upload

---
+++ assets/js/funciones.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('hamburger-btn');
+const mainMenu = document.getElementById('main-menu');
+
+menuToggle.addEventListener('click', function() {
+    mainMenu.classList.toggle('active');
+});
+
